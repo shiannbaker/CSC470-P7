ea50ccaa0766fa48ee4065104eb344dfdd5c91a8
elijah-ness-dsu <elijah.ness@trojans.dsu.edu>
2021-11-14 13:12:06 -0600

    Add interface file for IssueStatusRepository

---
+++ P5/IIssueStatusRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    interface IIssueStatusRepository
+    {
+        void Add(int Id, string value);
+        List<IssueStatus> GetAll();
+        int GetIdByStatus(string value);
+        string GetValueById(int Id);
+    }
+}
