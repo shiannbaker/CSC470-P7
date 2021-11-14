c15a46c2a44fb6ad4a745d90a0f82c753fb8917b
elijah-ness-dsu <elijah.ness@trojans.dsu.edu>
2021-11-14 13:20:17 -0600

    Add IIssueRepository interface and remove unused dependices in the IIssueStatusRepository interface

---
+++ P5/IIssueRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace P5
+{
+    interface IIssueRepository
+    {
+        string Add(Issue issue);
+        List<Issue> GetAll(int ProjectId);
+        bool Remove(Issue issue);
+        string Modify(Issue issue);
+        int GetTotalNumberOfIssues(int ProjectId);
+        List<string> GetIssuesByMonth(int ProjectId);
+        List<string> GetIssuesByDiscoverer(int ProjectId);
+        Issue GetIssueById(int Id);
+    }
+}
